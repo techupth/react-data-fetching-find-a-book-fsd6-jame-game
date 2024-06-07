53fc4effe7e15bf703ef957cbfebb9736b05ec70
tanayod <tanayod741@gmail.com>
2024-06-07 15:56:52 +0700

    feat:Data Fetching Find a Book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [serch, setSerch] = useState("");
+  const [title, setTitle] = useState([]);
+  const handleInput = (event) => {
+    setSerch(event.target.value);
+  };
+
+  const data = async () => {
+    const result =
+      await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${serch}
+`);
+    setTitle(result.data.items);
+  };
+
+  useEffect(() => {
+    data();
+  }, [serch]);
+
+  return (
+    <div className="App">
+      <h1>Find a book</h1>
+      <input type="text" value={serch} onChange={handleInput} />
+      <ul>
+        {title.map((item, index) => {
+          return <li key={index}>{item.volumeInfo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
